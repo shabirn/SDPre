765d5d205b47360a12bb31c8fcd5ceafac45e335
Shabir Nawabi <Shabir.n@gmail.com>
2023-02-16 21:24:38 -0800

    complete objective2 lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+		int yearBorn = 1982;
+		String monthBorn = "August";
+		boolean isWeekend = false;
+
+		System.out.println(yearBorn);
+		System.out.println(monthBorn);
+		System.out.println(isWeekend);
+
+	}
+}
