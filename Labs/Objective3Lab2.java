e26f71a8dc58f8c256f9e68a9e8f5f69a59e945a
Shabir Nawabi <Shabir.n@gmail.com>
2023-02-18 22:12:37 -0800

    completed Objective 3 Lab 2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,9 @@
+public class Objective3Lab2 {
+	public static void main(String[] args) {
+		double side1 = 10.0;
+		double side2 = 8.0;
+		double hypotenuse = Math.sqrt((Math.pow(side1, 2)+Math.pow(side2, 2)));
+		System.out.println(hypotenuse);
+
+	}
+}
